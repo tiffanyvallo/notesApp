63291e503a0d1d61b6ee2851bf910c6adc02ddcc
Jessica Brand <jeshbrand@gmail.com>
2021-05-11 16:32:04 +0100

    added back in a handy console log

--- src/interface.js
+++ src/interface.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", ()=> {
   var notesManager = new Manager();
 
-  document.querySelector('#noteInputTextArea')
   document.querySelector('#noteInput').addEventListener('submit',()=>{
     event.preventDefault();
 
     text = document.querySelector('#noteInputTextArea')
-    console.log(text.value)
+    // console.log(text.value)
     notesManager.create(new Note(`${text.value}`))
     console.log(notesManager)
   })
